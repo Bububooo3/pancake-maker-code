5a09554cb9337002cf3faf463b6887faa9c87a41
Richard Scott <bububooo3@gmail.com>
2025-11-04 11:00:19 -0500

    Integrate LED strip and refactor button handling
    Refactor subsystem controller to integrate LED strip and update button handling.

--- subsystem_controller.ino
+++ subsystem_controller.ino
@@ -1,36 +1,65 @@
+#include <Adafruit_NeoPixel.h>
+
 #include <LiquidCrystal.h>
 
-// Initialize LCD: (RS, E, D4, D5, D6, D7)
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-
 	// GLOBALS \\
-const char consoleMessages[] = {\
-	"Initializing",\
-  	"Select number",\
-  	"Bake?",\
-  	"",\
-};
-
 bool baking = false;
 bool prevConfirmState = HIGH;
 
-	// CONSTANTS \\
-// Buttons
-const int confirmPin = 7;
 
-// Miscellaneous
-const int confirmLEDPin = 9;
-const int conveyorMotorPin = 10;
+	// CONSTANTS \\
+// Counts
+#define LEDCOUNT 6
+
+// Buttons
+#define CONFIRMPIN 7
+#define LEDPIN 6
+#define MOTORPIN 10
+
+// Arrays
+const char intro[4][16] = {\
+	"Initializing",\
+  	"Select number",\
+  	"Bake how much?",\
+  	"",\
+};
+
+
+	// INITIALIZE COMPONENTS \\
+// Initialize LCD: (RS, E, D4, D5, D6, D7)
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+// Initialize LED Strip: (#LEDs, Pin, Color Mode + Signal)
+Adafruit_NeoPixel led(LEDCOUNT, LEDPIN, NEO_GRB + NEO_KHZ800);
+
+	// LIBRARY-SPECIFIC CONSTANTS \\
+// Colors
+#define WHITE strip.Color(255, 255, 255)
+#define RED strip.Color(255, 0, 0)
+#define GREEN strip.Color(0, 255, 0)
+#define BLUE strip.Color(0, 0, 255)
+
+#define YELLOW strip.Color(255, 255, 0)
+#define CYAN strip.Color(0, 255, 255)
+#define MAGENTA strip.Color(255, 0, 255)
+#define ORANGE strip.Color(255, 165, 0)
+#define PURPLE strip.Color(128, 0, 128)
+
+#define OFF strip.Color(0, 0, 0)
+
 
 	// FUNCTIONS \\
 // Little useful functions
-
 bool printMessage(String msg, int line = 0, int column = 0) {
   	lcd.setCursor(column, line);  
 	lcd.print("                ");
 	lcd.setCursor(column, line);
 	lcd.print(msg);
   	return true;
+}
+
+bool getConfirmState(){
+	return !(digitalRead(CONFIRMPIN));
 }
 
 bool clearLine(int lvl=-1){
@@ -43,8 +72,12 @@ bool clearLine(int lvl=-1){
   return true;
 }
 
-bool scrollLine(){
+bool scrollLine(int num=2){
 
+}
+
+bool setColors(int a=WHITE, int b=WHITE, int c=WHITE, int d=WHITE, int e=WHITE, int f=WHITE) {
+	led.SetPixelColor();
 }
 
 // Keyframe functions
@@ -60,9 +93,9 @@ void introductionProtocol(){
 // Run once on boot
 void setup() {
   // Buttons
-  pinMode(confirmPin, INPUT_PULLUP);
-  pinMode(confirmLEDPin, OUTPUT);
-  pinMode(conveyorMotorPin, OUTPUT);
+  pinMode(CONFIRMPIN, INPUT_PULLUP);
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(MOTORPIN, OUTPUT);
   
   // LCD
   lcd.begin(16,2);
@@ -78,14 +111,12 @@ void loop() {
   // Display on LCD
   printMessage(((map(analogRead(A0), 0, 1023, 1, 8)>1) ? level + " Pancakes" : level + " Pancake"), 1);
   
-  // Button handler
-  int confirmState = (!(digitalRead(confirmPin)));
+  // Button handler  
+  baking = (prevConfirmState == HIGH && getConfirmState() == LOW) ? !baking : baking;
   
-  baking = (prevConfirmState == HIGH && confirmState == LOW) ? !baking : baking;
+  digitalWrite(LEDPIN, !baking);
+  digitalWrite(MOTORPIN, !baking);
   
-  digitalWrite(confirmLEDPin, !baking);
-  digitalWrite(conveyorMotorPin, !baking);
-  
-  prevConfirmState = confirmState;
+  prevConfirmState = getConfirmState();
   delay(50);
 }
