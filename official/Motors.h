b7b5872c902948e97647f8e2f8a5974685c7fa05
Richard Scott <bububooo3@gmail.com>
2026-02-26 10:44:16 -0500

    Create Motors.h

---
+++ official/Motors.h
@@ -0,0 +1,18 @@
+#ifndef MOTORS_H
+#define MOTORS_H
+
+// Step pulse rate (steps/second)
+const int MAXSTEP = 800.0;
+
+const int CONVEYORSTEP = 800.0;
+
+const int DISPENSERSPEED = 800.0;
+
+const int DISPENSERACCEL = 400.0;
+
+// Step positions (Usually 1.8°/step, 200 steps/revolution)
+const int DISPENSEROPEN = 0;
+
+const int DISPENSERCLOSE = 0;
+
+#endif
