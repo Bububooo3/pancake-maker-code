b4c3a1b4d8398ab261d0b8cce31bc9b0bd3a2adf
Richard Scott <bububooo3@gmail.com>
2026-02-26 10:44:05 -0500

    Create Animations.h

---
+++ official/Animations.h
@@ -0,0 +1,39 @@
+#ifndef ANIMATIONS_H
+#define ANIMATIONS_H
+
+// Introduction animation
+const char intro[][16] = {
+  "pancake maker",
+  "pAncake maker",
+  "paNcake maker",
+  "panCake maker",
+  "pancAke maker",
+  "pancaKe maker",
+  "pancakE maker",
+  "pancake Maker",
+  "pancake mAker",
+  "pancake maKer",
+  "pancake makEr",
+  "pancake makeR",
+  " ",
+};
+
+// Heating animation
+const char heatingAnim[][16] = {
+  "heating",
+  "heating.",
+  "heating..",
+  "heating...",
+  "heating",
+};
+
+// Cooling animation
+const char coolingAnim[][16] = {
+  "cooling",
+  "cooling.",
+  "cooling..",
+  "cooling...",
+  "cooling",
+};
+
+#endif
