3c694d72e2a59487b8f1c3616c76e45f430612a0
Richard Scott <bububooo3@gmail.com>
2026-02-26 10:44:29 -0500

    Create Times.h

---
+++ official/Times.h
@@ -0,0 +1,17 @@
+#ifndef TIMES_H
+#define TIMES_H
+
+// (Miliseconds)
+const int HEATUP = 2e3; // Time it takes for the griddle to turn on
+
+const int COOLDOWN = 2e3; // Time it takes for the griddle to turn off
+
+const int KILLTIMEOUT = 60e3; // Maximum time allowed to remain in terminated state
+
+const int MSGWAIT = 1.5e3; // Intro message display time
+
+const int ANIMINC = 0.25e3; // Animation speed
+
+const int COOKTIME = 10e3; // Time for one pancake on the belt
+
+#endif
