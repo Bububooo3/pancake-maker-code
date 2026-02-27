8dc7d91dfc33149f359b115e1a5b2fe75d905cb5
Richard Scott <bububooo3@gmail.com>
2026-02-27 13:43:08 -0500

    Update Pins.h

--- official/Pins.h
+++ official/Pins.h
@@ -2,28 +2,22 @@
 #define PINS_H
 
 // Button pins
-const int CONFIRMPIN = 7; // Confirm
-const int CANCELPIN = 2; // Cancel
+const int CONFIRMPIN = 7;  // Confirm
+const int CANCELPIN = 2;   // Cancel
 
 
 // Appliance pins
-const int LEDPIN = 6; // For the NeoPixel strip
-const int GRIDPIN = 8; // For the SSR controlling the griddle
+const int LEDPIN = 6;   // For the NeoPixel strip
+const int GRIDPIN = 8;  // For the SSR controlling the griddle
 
 
 // Conveyor pins
-const int CONVEYORPIN_EN = 24; // Enable
-const int CONVEYORPIN_STEP = 22; // Step
-const int CONVEYORPIN_DIR = 26; // Direction
+const int CONVEYORPIN_STEP = 22;  // Step
+const int CONVEYORPIN_DIR = 26;   // Direction
 
 
 // Dispenser pins
-const int DISPENSERPIN_EN = 33; // Enable
-const int DISPENSERPIN_STEP = 35; // Step
-const int DISPENSERPIN_DIR = 37; // Direction
-
-
-// Fan pins <TODO>
-const int COOLINGPIN_EN = 27; // Enable
+const int DISPENSERPIN_STEP = 35;  // Step
+const int DISPENSERPIN_DIR = 37;   // Direction
 
 #endif
