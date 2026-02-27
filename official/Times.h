fe64d244a50c22fd030d247f0661cdb18b7cab92
Richard Scott <bububooo3@gmail.com>
2026-02-27 13:42:55 -0500

    Update Times.h

--- official/Times.h
+++ official/Times.h
@@ -2,16 +2,18 @@
 #define TIMES_H
 
 // (Miliseconds)
-const int HEATUP = 2e3; // Time it takes for the griddle to turn on
+const int HEATUP = 2e3;  // Time it takes for the griddle to turn on
 
-const int COOLDOWN = 2e3; // Time it takes for the griddle to turn off
+const int COOLDOWN = 2e3;  // Time it takes for the griddle to turn off
 
-const int KILLTIMEOUT = 60e3; // Maximum time allowed to remain in terminated state
+const int KILLTIMEOUT = 60e3;  // Maximum time allowed to remain in terminated state
 
-const int MSGWAIT = 1.5e3; // Intro message display time
+const int MSGWAIT = 1.5e3;  // Intro message display time
 
-const int ANIMINC = 0.25e3; // Animation speed
+const int ANIMINC = 0.25e3;  // Animation speed
 
-const int COOKTIME = 10e3; // Time for one pancake on the belt
+const int COOKTIME = 10e3;  // Time for one pancake on the belt
+
+const int DEBOUNCE = 3e3;  // Time before buttons sink input again after pressed
 
 #endif
