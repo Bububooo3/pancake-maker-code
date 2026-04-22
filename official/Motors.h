af6ecc3ef2229bc6a25e94846a2330dfeda779fd
Richard Scott <bububooo3@gmail.com>
2026-04-22 18:16:24 -0400

    Change motor constants from int to unsigned long

--- official/Motors.h
+++ official/Motors.h
@@ -2,15 +2,15 @@
 #define MOTORS_H
 
 // Step pulse rate (steps/second)
-const int MAXSTEP = 800.0;
+unsigned long MAXSTEP = 800.0;
 
-const int CONVEYORSTEP = 800.0;
+unsigned long CONVEYORSTEP = 800.0;
 
 const bool CONVEYORINVERT = true;
 
-const int DISPENSERSPEED = 800.0;
+unsigned long DISPENSERSPEED = 800.0;
 
-const int DISPENSERACCEL = 400.0;
+unsigned long DISPENSERACCEL = 400.0;
 
 // Step positions (Usually 1.8°/step, 200 steps/revolution)
 const int DISPENSEROPEN = 0;
