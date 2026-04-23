348d820e5566594562a661a0063b0a0b218cb1ef
Richard Scott <bububooo3@gmail.com>
2026-04-23 08:27:12 -0400

    Change DISPENSEROPEN step position offset
    Updated DISPENSEROPEN to reflect a 90° position offset.

--- official/Motors.h
+++ official/Motors.h
@@ -12,9 +12,10 @@ const unsigned long DISPENSERSPEED = 800.0;
 
 const unsigned long DISPENSERACCEL = 400.0;
 
-// Step positions (Usually 1.8°/step, 200 steps/revolution)
-const int DISPENSEROPEN = 0;
 
+
+// Step position offsets (Usually 1.8°/step, 200 steps/revolution)
+const int DISPENSEROPEN = 50; // 90° (may need to invert)
 const int DISPENSERCLOSE = 0;
 
 #endif
