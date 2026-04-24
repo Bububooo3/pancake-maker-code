c316b65c64d9b3697981ee29ce4d5a9b4bd524fb
Richard Scott <bububooo3@gmail.com>
2026-04-24 15:34:57 -0400

    Refactor motor constants to use const float

--- official/Motors.h
+++ official/Motors.h
@@ -2,20 +2,22 @@
 #define MOTORS_H
 
 // Step pulse rate (steps/second)
-unsigned long MAXSTEP = 800.0;
+const float MAXSTEP = 800.0;
 
-unsigned long CONVEYORSTEP = 800.0;
+const float CONVEYORSTEP = 800.0;
 
 const bool CONVEYOR_INVERT = true;
 
-const unsigned long DISPENSERSPEED = 800.0;
+const bool DISPENSER_INVERT = false;
 
-const unsigned long DISPENSERACCEL = 400.0;
+const float DISPENSERSPEED = 80.0;
+
+const float DISPENSERACCEL = 800.0;
 
 
 
 // Step position offsets (Usually 1.8°/step, 200 steps/revolution)
-const int DISPENSEROPEN = 50; // 90° (may need to invert)
+const int DISPENSEROPEN = -60 * (5.0/9.0); // degrees * -5/9
 const int DISPENSERCLOSE = 0;
 
 #endif
