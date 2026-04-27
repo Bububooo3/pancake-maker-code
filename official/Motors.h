830a93195677589cf9ef4a4bb43be65a7a409cf6
Richard Scott <bububooo3@gmail.com>
2026-04-26 22:15:14 -0400

    Update Motors.h

--- official/Motors.h
+++ official/Motors.h
@@ -4,20 +4,20 @@
 // Step pulse rate (steps/second)
 const float MAXSTEP = 800.0;
 
-const float CONVEYORSTEP = 800.0;
+const float CONVEYORSTEP = 350.0;
 
 const bool CONVEYOR_INVERT = true;
 
 const bool DISPENSER_INVERT = false;
 
-const float DISPENSERSPEED = 80.0;
+const float DISPENSERSPEED = 25.0;
 
-const float DISPENSERACCEL = 800.0;
+const float DISPENSERACCEL = 100.0;
 
 
 
 // Step position offsets (Usually 1.8°/step, 200 steps/revolution)
-const int DISPENSEROPEN = -60 * (5.0/9.0); // degrees * -5/9
+const int DISPENSEROPEN = -70 * (5.0/9.0); // degrees * -5/9
 const int DISPENSERCLOSE = 0;
 
 #endif
