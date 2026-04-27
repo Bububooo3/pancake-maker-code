9e390f8bad21edbf119ee183aa02c4845fb80838
Richard Scott <bububooo3@gmail.com>
2026-04-26 22:16:10 -0400

    Update Times.h

--- official/Times.h
+++ official/Times.h
@@ -2,9 +2,9 @@
 #define TIMES_H
 
 // (Miliseconds)
-const unsigned long HEATUP = 2e3;  // Time it takes for the griddle to turn on
+const unsigned long HEATUP = 40e3;  // Time it takes for the griddle to turn on
 
-const unsigned long COOLDOWN = 2e3;  // Time it takes for the griddle to turn off
+const unsigned long COOLDOWN = 40e3;  // Time it takes for the griddle to turn off
 
 const unsigned long KILLTIMEOUT = 60e3;  // Maximum time allowed to remain in terminated state
 
@@ -12,7 +12,7 @@ const unsigned long MSGWAIT = 1.5e3;  // Intro message display time
 
 const unsigned long ANIMINC = 0.25e3;  // Animation speed
 
-const unsigned long COOKTIME = 60e3;  // Time for one pancake on the belt
+const unsigned long COOKTIME = 80e3;  // Time for one pancake on the belt
 
 const unsigned long DEBOUNCE = .025e3;  // Time before buttons sink input again after pressed
 
